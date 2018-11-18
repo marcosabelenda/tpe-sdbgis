2446c6b5b135dfed8ccb489e67c5b3e992a12ad5
jpascale <jpascale@itba.edu.ar>
2018-11-18 17:39:43 -0300

    Add connection module && Update README

--- README.md
+++ README.md
@@ -19,6 +19,27 @@ $ mongo tpgeodb
 db.messages.createIndex( { geometry : "2dsphere" } )
 ```
 
+## Configure postgres
+```
+CREATE DATABASE sdbgis;
+CREATE EXTENSION postgis;
+
+CREATE TABLE tweets (
+  id serial primary key,
+  geometry  geometry(Point,4326) ,
+  username  varchar(100),
+  message   varchar(300),
+  range     int
+);
+
+```
+
+Insertion to DB is of type:
+
+```
+INSERT INTO tweets(geometry, username, message, range) VALUES (st_SetSrid(st_MakePoint(-58.491008, -34.5008638), 4326), 'test', 'tweet de prueba', 3000);
+```
+
 ## Run the program
 
 ```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const async = require('async')
-  , { connect: mongoConnect } = require('./storage/mongo');
-
+  , { connect: mongoConnect } = require('./storage/mongo')
+  , psql = require('./storage/postgres');
 
 const mongoConnectWrapper = (cb) => {
   // TODO: Put in config file
@@ -8,8 +8,20 @@ const mongoConnectWrapper = (cb) => {
   mongoConnect(mongoURI, cb);
 }
 
+const postgresConnectWrapper = (cb) => {
+  const postgresConfig = {
+    user: 'jpascale',
+    host: 'localhost',
+    database: 'sdbgis',
+    password: '',
+    port: 5432,
+  }
+  psql.config(postgresConfig, cb);
+}
+
 async.series([
-  mongoConnectWrapper
+  mongoConnectWrapper,
+  postgresConnectWrapper
 ], (err) => {
   if (err) {
     console.error('BANG! 💥💥💥💥 Failed startup process.');
